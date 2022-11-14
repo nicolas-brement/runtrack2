bf6058233fa5eb1adfd0bff2da6148b6e7b23942
nicolas-brement <nicolas.brement@laplateforme.io>
2022-11-14 15:17:02 +0100

    Ajout du job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<p>". "Hello LaPlateforme!" . "</p>"; // Ceci est un premier commentaire sur une ligne
+                                            # Ceci est un second commentaire sur une ligne
+
+echo "Hello LaPlateforme!"; /*
+                            Ceci est un commentaire en bloc
+
+                            La saisie du commentaire se terminera lorsque nous le fermeront avec étoile slash
+                            */
