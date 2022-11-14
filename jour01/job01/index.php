8829ca4b47e2fb724bbf66922446c0bdcc274978
nicolas-brement <nicolas.brement@laplateforme.io>
2022-11-14 14:54:06 +0100

    Ajout du job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,6 +1,14 @@
 <?php
 $str = "LaPlateforme ";
+echo "<p>".$str."</p>";
+
 $str2 = "Vive ";
 $str3 = "!";
+echo "<p>".$str2 . $str . $str3."</p>";
 
-echo ($str2 . $str . $str3);
+$val = "6";
+echo "<p>".($val + 4)."</p>";
+
+$myBool = false;
+echo $myBool;
+
