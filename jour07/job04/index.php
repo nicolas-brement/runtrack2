fdfa6757c531db36f08a42041a12b9541e359eaa
nicolas-brement <nicolas.brement@laplateforme.io>
2022-11-22 15:25:31 +0100

    Ajout du job04

---
+++ jour07/job04/index.php
@@ -0,0 +1,32 @@
+<?php
+
+function calcule($a, $operation, $b)
+{
+    $resultat = 0;
+    {
+        if ($operation == '+') 
+            $resultat = ($a + $b);
+
+        elseif ($operation == '-')
+            $resultat = ($a - $b);
+        
+        elseif ($operation == '*')
+            $resultat = ($a * $b);
+
+        elseif ($operation == '/')
+            $resultat = ($a / $b);
+
+        elseif ($operation == '%')
+            $resultat == ($a % $b);
+    }      
+
+return $resultat;
+}
+
+echo calcule(13,'+',13) . "<br/>";
+echo calcule(26,'-',13) . "<br/>";
+echo calcule(6,'*',4) . "<br/>";
+echo calcule(10,'/',2) . "<br/>";
+echo calcule(10,'%',100) . "<br/>"; // la division euclidienne "%" ne semble pas être prise en compte
+
+?>
