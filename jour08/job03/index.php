5285869876d5e0ccfc89f8a3ce53e027ea3fcd6d
nicolas-brement <nicolas.brement@laplateforme.io>
2022-11-23 14:58:48 +0100

    Ajout du job03

--- jour08/job03/index.php
+++ jour08/job03/index.php
@@ -1,5 +1,31 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['name']) || isset($_POST['reset'])){
+    $_SESSION['name']=[];
+}
+?>
+
 <form method="post">
-    Entrez votre nom <br>
-    <input type="text" name="username">
-    <input type="submit" name="reset" value="Renitialiser le compteur">
+    Entrez votre prénom : <br>
+    <input type="text" name="name">
+    <input type="submit" value="Ajout"> <br><br>
+    <input type="submit" name="reset" value="Renitialiser la liste">
+</form>
+
+<?php
+    if(isset($_POST['name'])){
+        $_SESSION['name'][] = $_POST['name'];
+
+        echo '<ul>';
+        foreach($_SESSION['name'] as $name){
+            echo "<li>$name</li>";
+        }
+        echo '</ul>';
+    }
+?>
+
+
+
+
 
